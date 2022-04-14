660824f7fce29a1a2dc672f008d9947f60ef4542
zdenham <zdenham1@gmail.com>
2022-04-14 09:59:04 -0700

    main

--- src/svelte/parts/GenericButton.svelte
+++ src/svelte/parts/GenericButton.svelte
@@ -1,18 +1,34 @@
 <script lang="ts">
+  type StyleType = 'primary' | 'secondary';
   export let handleClick: () => void;
   export let title = 'Connect Wallet';
+  export let styleType: StyleType = 'primary';
 </script>
 
-<button class="connectButton" on:click={handleClick}>{title}</button>
+<button
+  class={styleType === 'primary'
+    ? 'connectButton primary'
+    : 'connectButton secondary'}
+  on:click={handleClick}>{title}</button
+>
 
 <style>
   .connectButton {
-    background: black;
-    color: white;
     padding: 15px 30px;
     cursor: pointer;
     border: none;
     border-radius: 7px;
     font-size: 16px;
+  }
+
+  .primary {
+    background: black;
+    color: white;
+  }
+
+  .secondary {
+    color: black;
+    background: white;
+    border: 1px solid black;
   }
 </style>
---
+++ src/svelte/parts/LoaderButton.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import GenericButton from './GenericButton.svelte';
+  import RotatorBorder from './RotatorBorder.svelte';
+
+  export let isLoading = false;
+  export let loadingText = 'minting...';
+  export let title = 'Mint';
+  export let handleClick;
+</script>
+
+<RotatorBorder>
+  <GenericButton
+    title={isLoading ? loadingText : title}
+    {handleClick}
+    styleType={isLoading ? 'secondary' : 'primary'}
+  />
+</RotatorBorder>
+
+<style>
+</style>
