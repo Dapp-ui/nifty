6b32a61bc557179a04ac56e567d0aa5481c0f2f3
zdenham <zdenham1@gmail.com>
2022-04-15 12:06:36 -0700

    render inside of shadow roots

--- src/svelte/main.ts
+++ src/svelte/main.ts
@@ -4,19 +4,36 @@ import ConnectWallet from './ConnectWalletFlow.svelte';
 import MintFlow from './MintFlow.svelte';
 import ErrorBanner from './parts/ErrorBanner.svelte';
 
+// error container
+const errorContainer = document.createElement('div');
+document.body.appendChild(errorContainer);
+
+// find all handles
 const walletConnector = document.getElementById('connectWalletButton');
 const mintButton = document.getElementById('mintButton');
 
+const connectorShadow = walletConnector.attachShadow({ mode: 'open' });
+const mintShadow = mintButton.attachShadow({ mode: 'open' });
+const errorShadow = errorContainer.attachShadow({ mode: 'open' });
+
+const walletConnectTarget = document.createElement('div');
+const mintButtonTarget = document.createElement('div');
+const errorTarget = document.createElement('div');
+
+connectorShadow.appendChild(walletConnectTarget);
+mintShadow.appendChild(mintButtonTarget);
+errorShadow.appendChild(errorTarget);
+
 const connectWallet = new ConnectWallet({
-  target: walletConnector,
+  target: walletConnectTarget,
 });
 
 const mint = new MintFlow({
-  target: mintButton,
+  target: mintButtonTarget,
 });
 
 const errorText = new ErrorBanner({
-  target: document.body,
+  target: errorTarget,
 });
 
 export default { connectWallet, mint, errorText };
