2732c5d091a8c5ec16a04159b2136b5e720f2cf4
zdenham <zdenham1@gmail.com>
2022-05-13 17:49:43 -0700

    move in diamond

---
+++ test/contract/PaymentSplitter.test.ts
@@ -0,0 +1,55 @@
+import { getSelectors, FacetCutAction } from "../scripts/libraries/diamond.js";
+require("@nomiclabs/hardhat-waffle");
+
+const { deployDiamond } = require("../scripts/deploy.js");
+
+const { assert, expect } = require("chai");
+const { ethers } = require("hardhat");
+
+const cutAbi = require("../artifacts/contracts/facets/DiamondClone/DiamondCloneCutFacet.sol/DiamondCloneCutFacet.json");
+
+describe("AccessControlTest", async function () {
+  let diamondAddress,
+    initCallData,
+    sawInstance,
+    baseNFTFacetImplementation,
+    paymentSplitter,
+    contractOwner,
+    accounts;
+
+  beforeEach(async function () {
+    const data = await deployDiamond();
+    diamondAddress = data.diamondAddress;
+    initCallData = data.initCallData;
+    sawInstance = data.sawInstance;
+    baseNFTFacetImplementation = data.baseNFTFacetImplementation;
+    paymentSplitter = await ethers.getContractAt(
+      "PaymentSplitterFacet",
+      diamondAddress
+    );
+    accounts = await ethers.getSigners();
+    contractOwner = accounts[0];
+  });
+
+  it("should withdraw appropriate basis points after setting payment splits", async () => {
+    paymentSplitter.set;
+    expect(false).to.equal(true);
+  });
+
+  it("should withdraw appropriate basis points after updating payment splits", async () => {
+    expect(false).to.equal(true);
+  });
+
+  it("Should reject incorrect basis points", async () => {
+    // TODO -
+    expect(false).to.equal(true);
+  });
+
+  it("Should reject modification of the dev basis points", async () => {
+    expect(false).to.equal(true);
+  });
+
+  it("Should reject modifications if payment splitter is locked", async () => {
+    expect(false).to.equal(true);
+  });
+});
